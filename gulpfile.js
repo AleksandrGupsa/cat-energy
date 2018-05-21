2923a62359cd15726ca7a626d2560c6fa4c66bcb
Гупса Александр И <aleksandr.gupsa@sokolov.ru>
2018-05-21 17:59:19 +0300

    some shit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 'use strict';
-
+ 
 const gulp = require('gulp');
-
-gulp.task('hello', function(){
-    console.log('Hello');
+const sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
